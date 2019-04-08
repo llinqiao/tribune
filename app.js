05a84bd96af15a59b5b0bf36112010fc97adb9ec
linqiao <984673705@qq.com>
2019-04-08 16:10:18 +0800

    templates finish

--- app.js
+++ app.js
@@ -40,68 +40,49 @@ const posts = [{
   timestamp: Date.now()
 }]
 
+app.locals.pretty = true
+
+app.set('view engine', 'pug')//设置使用的模板引擎
+app.set('views', __dirname + '/templates')//设置模板文件的文件夹
 
 app.use(express.json())//express自动解析请求体
 app.use(express.urlencoded())
 app.use(cookieParser())
 
-app.use((req,res,next) => {
-  if(req.cookies.user){
-    req.user = users.find(it => it.name ==req.cookies.user)
+app.use((req, res, next) => {
+  if (req.cookies.user) {
+    req.user = users.find(it => it.name == req.cookies.user)
   }
   next()
-})//通过cookies来找到
+})//通过cookies来找到用户
 
 app.get('/', (req, res, next) => {
-  res.send(`
-    <!doctype html>
-    <header>
-      
-      ${
-        req.user ?
-          `<a href ='/add-thread'>发布</a>
-          <a href="/logout">退出</a>`
-          :
-          `<a href="/login">登录</a>
-          <a href="/register">注册</a>`
-      }
-    
-      
-    </header>
-    <ul>
-      ${posts.map(post => {
-    return `
-          <li><a href="/post/${post.id}">${post.title}</a><small>${moment(post.timestamp).fromNow()}</li>
-        `
-  }).join('')}
-    </ul>
-  `)
+  res.render('index', {
+    user: req.user,
+    posts: posts,
+    moment: moment,
+  })
 })//首页
 
 
 
 app.get('/post/:id', (req, res, next) => {
   var post = posts.find(it => it.id == req.params.id)
-  res.send(`
-    <div>
-      <h1>${post.title}</h1>
-      <article>${post.content}</article>
-  `)
+  res.render('post', {
+    post: post,
+    user: req.user,
+  })
 })//根据id返回每个用户的页面
 
 
 app.route('/add-thread')
-  .get((req,res,next)=> {
-    res.send(`
-      <form action= "/add-thread" method = "post">
-        <input type ="text" name ="title"/>
-        <textarea name ="content"></textarea> 
-        <button>发布帖子</button>
-      </form>
-    `)
+  .get((req, res, next) => {
+    res.render('add-thread.pug', {
+      user: req.user,
+    })
   })
-  .post((req,res,next)=>{
-    if(req.user){//有用户登录才可以发
+  .post((req, res, next) => {
+    if (req.user) {//有用户登录才可以发
       var thread = req.body
       var lastThread = posts[posts.length - 1]
       thread.timestamp = Date.now()
@@ -110,66 +91,64 @@ app.route('/add-thread')
       posts.push(thread)
 
       res.redirect('/post/' + thread.id)
-    }else{
+    } else {
       res.send('未登录')
     }
   })//发布帖子
 
 app.route('/register')
   .get((req, res, next) => {
-    res.send(`
-      <form action ="/register" method ="post">
-        <input type = "text" name = "name" />
-        <input type = "password" name = "password"/>
-        <button>注册</button>
-      </form>
-    `)
+    res.render('register.pug')
   })
   .post((req, res, next) => {
     if (users.find(it => it.name == req.body.name) == null) {
       var lastUser = users[users.length - 1]
       req.body.id = lastUser.id + 1
       users.push(req.body)
+      res.cookie('user', req.body.name, {
 
-      res.send('注册成功')
+      })
+      res.render('register-result.pug', {
+        user: req.body,
+        status: 'SUCCESS'
+      })
     } else {
-      res.send('用户名已被占用')
+      res.render('register-result.pug', {
+        
+        status: 'USERNAME_USED'
+      })
     }
 
   })//注册页面
 
 
 
-  app.route('/login')
+app.route('/login')
   .get((req, res, next) => {
-    res.send(`
-      <form action ="/login" method ="post">
-        <input type = "text" name = "name"/>
-        <input type = "password" name = "password"/>
-        <button>登录</button>
-      </form>
-    `)
+    res.render('login')
+
   })
   .post((req, res, next) => {
     var user = users.find(it => it.name == req.body.name)
-    if(user){
-      if(user.password == req.body.password){
-        res.cookie('user',user.name,{
-          expires:new Date(Date.now()+86400000)
+    if (user) {
+      if (user.password == req.body.password) {
+        res.cookie('user', user.name, {
+          expires: new Date(Date.now() + 86400000),
+
         })
         res.redirect('/')//登录成功页面跳转到首页
-      }else{
+      } else {
         res.send('密码错误')
       }
-    }else{
+    } else {
       res.send('用户不存在')
     }
   })
 
-  app.get('/logout',(req,res,next) =>{
-    res.clearCookie('user')
-    res.redirect('/')
-  })
+app.get('/logout', (req, res, next) => {
+  res.clearCookie('user')
+  res.redirect('/')
+})
 
 
 
